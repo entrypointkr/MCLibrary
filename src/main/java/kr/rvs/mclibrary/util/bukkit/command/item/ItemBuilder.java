0be155db90126fd4f06aee9a34e4e0ac6b1383db
Junhyeong Lim <entrypoint_@naver.com>
2017-07-27 12:44:43 +0900

    Add item utils

---
+++ src/main/java/kr/rvs/mclibrary/util/bukkit/command/item/ItemBuilder.java
@@ -0,0 +1,75 @@
+package kr.rvs.mclibrary.util.bukkit.command.item;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+/**
+ * Created by Junhyeong Lim on 2017-07-27.
+ */
+public class ItemBuilder {
+    private final Material material;
+    private int amount = 1;
+    private short data = 0;
+
+    private String displayName = null;
+    private String[] lores = null;
+
+    public ItemBuilder(Material material) {
+        this.material = material;
+    }
+
+    public ItemBuilder(Material material, int amount) {
+        this.material = material;
+        this.amount = amount;
+    }
+
+    public ItemBuilder(Material material, short data) {
+        this.material = material;
+        this.data = data;
+    }
+
+    public ItemBuilder(Material material, int amount, short data) {
+        this.material = material;
+        this.amount = amount;
+        this.data = data;
+    }
+
+    public ItemBuilder amount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public ItemBuilder data(short data) {
+        this.data = data;
+        return this;
+    }
+
+    public ItemBuilder name(String displayName) {
+        this.displayName = displayName;
+        return this;
+    }
+
+    public ItemBuilder lore(String... lores) {
+        this.lores = lores;
+        return this;
+    }
+
+    public ItemStack build() {
+        ItemStack itemStack = new ItemStack(material, amount, data);
+        ItemMeta meta = itemStack.getItemMeta();
+
+        if (displayName != null)
+            meta.setDisplayName(displayName);
+        if (lores != null)
+            meta.setLore(Arrays.asList(lores));
+
+        return itemStack;
+    }
+
+    public ItemWrapper buildAndWrapping() {
+        return new ItemWrapper(build());
+    }
+}
---
+++ src/main/java/kr/rvs/mclibrary/util/bukkit/command/item/ItemWrapper.java
@@ -0,0 +1,47 @@
+package kr.rvs.mclibrary.util.bukkit.command.item;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Junhyeong Lim on 2017-07-27.
+ */
+public class ItemWrapper {
+    private final ItemStack itemStack;
+
+    public ItemWrapper(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public ItemWrapper setLore(String... lores) {
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setLore(Arrays.asList(lores));
+        itemStack.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemWrapper addLore(String... lores) {
+        ItemMeta meta = itemStack.getItemMeta();
+        List<String> loreList = meta.getLore();
+        loreList.addAll(Arrays.asList(lores));
+        itemStack.setItemMeta(meta);
+
+        return this;
+    }
+
+    public ItemWrapper setName(String displayName) {
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(displayName);
+        itemStack.setItemMeta(meta);
+
+        return this;
+    }
+}
