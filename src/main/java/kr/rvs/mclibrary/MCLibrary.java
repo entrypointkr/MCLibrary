382a73bcf8d927d922e114fef2df1e2b309db8d6
Junhyeong Lim <entrypoint_@naver.com>
2017-08-20 18:53:40 +0900

    Remove deprecated class

--- src/main/java/kr/rvs/mclibrary/MCLibrary.java
+++ src/main/java/kr/rvs/mclibrary/MCLibrary.java
@@ -2,7 +2,6 @@ package kr.rvs.mclibrary;
 
 import kr.rvs.mclibrary.util.bukkit.command.CommandManager;
 import kr.rvs.mclibrary.util.bukkit.inventory.GUI;
-import kr.rvs.mclibrary.util.bukkit.inventory.GUIHelper;
 import kr.rvs.mclibrary.util.gson.GsonManager;
 import kr.rvs.mclibrary.util.gson.SettingsManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -39,7 +38,6 @@ public class MCLibrary extends JavaPlugin {
     @Override
     public void onEnable() {
         GUI.init();
-        GUIHelper.init();
     }
 
     @Override
