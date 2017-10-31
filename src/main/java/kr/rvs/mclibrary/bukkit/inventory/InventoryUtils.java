f769d297d23b8aa54aecaddbb6e01cc1f12ed503
Junhyeong Lim <entrypoint_@naver.com>
2017-11-01 00:28:49 +0900

    Fix PlayerUtils

--- src/main/java/kr/rvs/mclibrary/bukkit/inventory/InventoryUtils.java
+++ src/main/java/kr/rvs/mclibrary/bukkit/inventory/InventoryUtils.java
@@ -1,6 +1,8 @@
 package kr.rvs.mclibrary.bukkit.inventory;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
@@ -22,5 +24,85 @@ public class InventoryUtils {
         for (Map.Entry<Integer, ItemStack> entry : contents.entrySet()) {
             inv.setItem(entry.getKey(), entry.getValue());
         }
+    }
+
+    public static int hasItems(Inventory inv, ItemStack item) {
+        int hasAmount = 0;
+        for (ItemStack elemItem : inv) {
+            if (elemItem == null || elemItem.getType() == Material.AIR
+                    || !elemItem.isSimilar(item))
+                continue;
+            hasAmount += elemItem.getAmount();
+        }
+        return hasAmount;
+    }
+
+    public static int hasItems(InventoryHolder holder, ItemStack item) {
+        return hasItems(holder.getInventory(), item);
+    }
+
+    public static boolean hasItem(Inventory inv, ItemStack item, int amount) {
+        int hasAmount = 0;
+        for (ItemStack elemItem : inv) {
+            if (elemItem == null || elemItem.getType() == Material.AIR
+                    || !elemItem.isSimilar(item))
+                continue;
+            hasAmount += elemItem.getAmount();
+            if (hasAmount >= amount)
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean hasItem(InventoryHolder holder, ItemStack item, int amount) {
+        return hasItem(holder.getInventory(), item, amount);
+    }
+
+    public static boolean takeItem(Inventory inv, ItemStack item, int takeAmount) {
+        Map<Integer, ItemStack> removeItemMap = new HashMap<>();
+        for (int i = 0; i < inv.getSize(); i++) {
+            ItemStack elemItem = inv.getItem(i);
+            if (elemItem == null || elemItem.getType() == Material.AIR
+                    || !elemItem.isSimilar(item))
+                continue;
+            int amount = elemItem.getAmount();
+            if (takeAmount > amount) {
+                takeAmount -= amount;
+                removeItemMap.put(i, elemItem);
+            } else {
+                if (takeAmount < amount) {
+                    elemItem.setAmount(amount - takeAmount);
+                    inv.setItem(i, elemItem);
+                } else {
+                    removeItemMap.put(i, elemItem);
+                }
+                for (Integer slot : removeItemMap.keySet()) {
+                    inv.clear(slot);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean takeItem(InventoryHolder holder, ItemStack item, int takeAmount) {
+        return takeItem(holder.getInventory(), item, takeAmount);
+    }
+
+    public static boolean hasSpace(Inventory inv, ItemStack item, int amount) {
+        int space = 0;
+        for (int i = 0; i < inv.getSize(); i++) {
+            ItemStack element = inv.getItem(i);
+            if (element == null || element.getType() == Material.AIR) {
+                space += item.getMaxStackSize();
+            } else if (element.isSimilar(item)) {
+                space += item.getMaxStackSize() - element.getAmount();
+            }
+        }
+        return space >= amount;
+    }
+
+    public static boolean hasSpace(InventoryHolder holder, ItemStack item, int amount) {
+        return hasSpace(holder.getInventory(), item, amount);
     }
 }
--- src/main/java/kr/rvs/mclibrary/bukkit/player/PlayerUtils.java
+++ src/main/java/kr/rvs/mclibrary/bukkit/player/PlayerUtils.java
@@ -4,20 +4,15 @@ import kr.rvs.mclibrary.Static;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -51,69 +46,6 @@ public class PlayerUtils {
 
             return onlinePlayers;
         }
-    }
-
-    public static int hasItems(Player player, ItemStack item) {
-        int hasAmount = 0;
-        for (ItemStack elemItem : player.getInventory()) {
-            if (elemItem == null || elemItem.getType() == Material.AIR
-                    || !elemItem.isSimilar(item))
-                continue;
-
-            hasAmount += elemItem.getAmount();
-        }
-
-        return hasAmount;
-    }
-
-    public static boolean isHasItem(Player player, ItemStack item, int amount) {
-        int hasAmount = 0;
-        for (ItemStack elemItem : player.getInventory()) {
-            if (elemItem == null || elemItem.getType() == Material.AIR
-                    || !elemItem.isSimilar(item))
-                continue;
-
-            hasAmount += elemItem.getAmount();
-
-            if (hasAmount >= amount)
-                return true;
-        }
-
-        return false;
-    }
-
-    public static boolean takeItem(Player player, ItemStack item, int takeAmount) {
-        Inventory inv = player.getInventory();
-        Map<Integer, ItemStack> removeItemMap = new HashMap<>();
-
-        for (int i = 0; i < inv.getSize(); i++) {
-            ItemStack elemItem = inv.getItem(i);
-            if (elemItem == null || elemItem.getType() == Material.AIR
-                    || !elemItem.isSimilar(item))
-                continue;
-
-            int amount = elemItem.getAmount();
-
-            if (takeAmount > amount) {
-                takeAmount -= amount;
-                removeItemMap.put(i, elemItem);
-            } else {
-                if (takeAmount < amount) {
-                    elemItem.setAmount(amount - takeAmount);
-                    inv.setItem(i, elemItem);
-                } else {
-                    removeItemMap.put(i, elemItem);
-                }
-
-                for (Integer slot : removeItemMap.keySet()) {
-                    inv.clear(slot);
-                }
-
-                return true;
-            }
-        }
-
-        return false;
     }
 
     public static Optional<Player> getPlayerOptional(String name) {
