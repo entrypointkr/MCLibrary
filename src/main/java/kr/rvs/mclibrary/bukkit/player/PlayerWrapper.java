2817d613f0ef06687a4ce6da894bda51bac858e5
Junhyeong Lim <entrypoint_@naver.com>
2017-11-01 00:35:28 +0900

    Fix junit test

--- src/main/java/kr/rvs/mclibrary/bukkit/player/PlayerWrapper.java
+++ src/main/java/kr/rvs/mclibrary/bukkit/player/PlayerWrapper.java
@@ -1,5 +1,6 @@
 package kr.rvs.mclibrary.bukkit.player;
 
+import kr.rvs.mclibrary.bukkit.inventory.InventoryUtils;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,15 +18,19 @@ public class PlayerWrapper {
     }
 
     public int hasItems(ItemStack item) {
-        return PlayerUtils.hasItems(player, item);
+        return InventoryUtils.hasItems(player, item);
     }
 
-    public boolean isHasItem(ItemStack item, int amount) {
-        return PlayerUtils.isHasItem(player, item, amount);
+    public boolean hasItem(ItemStack item, int amount) {
+        return InventoryUtils.hasItem(player, item, amount);
     }
 
     public boolean takeItem(ItemStack item, int takeAmount) {
-        return PlayerUtils.takeItem(player, item, takeAmount);
+        return InventoryUtils.takeItem(player, item, takeAmount);
+    }
+
+    public boolean hasSpace(ItemStack item, int amount) {
+        return InventoryUtils.hasSpace(player, item, amount);
     }
 
     public void sendBaseComponent(BaseComponent component) {
