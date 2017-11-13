cdfd379b2e5275c9f084f5ada2fd896fb52ac180
Junhyeong Lim <entrypoint_@naver.com>
2017-11-14 01:57:06 +0900

    Fix Cooldown

--- src/main/java/kr/rvs/mclibrary/general/Cooldown.java
+++ src/main/java/kr/rvs/mclibrary/general/Cooldown.java
@@ -6,11 +6,11 @@ import java.util.concurrent.TimeUnit;
  * Created by Junhyeong Lim on 2017-09-02.
  */
 public class Cooldown {
-    private final long cooltime;
+    private final long coolTime;
     private long lastCount = 0;
 
     public Cooldown(TimeUnit unit, int number) {
-        this.cooltime = unit.toMillis(number);
+        this.coolTime = unit.toMillis(number);
     }
 
     public boolean isAfter() {
@@ -18,7 +18,7 @@ public class Cooldown {
     }
 
     public void cooldown() {
-        lastCount = System.currentTimeMillis() + cooltime;
+        lastCount = System.currentTimeMillis() + coolTime;
     }
 
     public boolean count() {
@@ -29,8 +29,20 @@ public class Cooldown {
         return false;
     }
 
-    public long cooltime(TimeUnit unit) {
+    public long remainCooltime(TimeUnit unit) {
         long currTime = System.currentTimeMillis();
         return unit.convert(lastCount > currTime ? lastCount - currTime : 0, TimeUnit.MILLISECONDS);
+    }
+
+    public long remainCooltime() {
+        return remainCooltime(TimeUnit.SECONDS);
+    }
+
+    public long cooltime(TimeUnit unit) {
+        return unit.convert(coolTime, TimeUnit.MILLISECONDS);
+    }
+
+    public long cooltime() {
+        return cooltime(TimeUnit.SECONDS);
     }
 }
